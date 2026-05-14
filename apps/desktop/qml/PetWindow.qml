3c947bb154ee5d36f1d53d375bb42b495debd9b3
tiantian180 <904543268@qq.com>
2026-05-15 03:11:01 +0800

    build: add v2 desktop shell project

---
+++ apps/desktop/qml/PetWindow.qml
@@ -0,0 +1,10 @@
+import QtQuick
+import QtQuick.Window
+
+Window {
+    width: 240
+    height: 240
+    visible: true
+    color: "transparent"
+    title: "MilesEdgeworth v2"
+}
---
+++ apps/desktop/src/main.cpp
@@ -0,0 +1,19 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+
+    engine.loadFromModule("MilesEdgeworth", "PetWindow");
+
+    return app.exec();
+}
